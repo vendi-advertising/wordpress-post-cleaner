74c38e72d51d9ffffc9c38f8add4fac82dc82df5
Chris Haas <chris@vendiadvertising.com>
2018-01-05 16:18:35 -0600

    Deleted legacy app.php file

--- app.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-define( 'VENDI_LCT_FILE', __FILE__ );
-define( 'VENDI_LCT_PATH', dirname( __FILE__ ) );
-
-require_once VENDI_LCT_PATH . '/vendor/autoload.php';
-
-
-WP_CLI::add_command( 'lct',        \Vendi\LCT\cleaner::class );
